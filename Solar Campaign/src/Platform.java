4253dad34dd0486c04a6842c7cb39c7f39896824
ASDISAGFJ <153211880+ASDISAGFJ@users.noreply.github.com>
2023-12-15 10:36:29 -0500

    Platform.java

--- Solar Campaign/src/Platform.java
+++ Solar Campaign/src/Platform.java
@@ -1,15 +1,13 @@
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.paint.ImagePattern;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class Platform extends Rectangle {
-    public Platform(double x, double y, double width, double height, Image image) {
-        super(x, y, width, height);
-        ImageView objectView = new ImageView(image);
-    }
 
-    public void setImage(Image image) {
-        setFill(new ImagePattern(image));
+    public Platform(double x, double y, double width, double height) {
+        super(x, y, width, height);
+        setFill(Color.GREY); // You can customize the color
+    }
+    public void setPlatformVisible(boolean visible) {
+        this.setVisible(visible);
     }
 }
